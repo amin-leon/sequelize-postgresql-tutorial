f6812c0600fba07985921d3cc91425814fd207fd
NPL Codes <104311071+amin-leon@users.noreply.github.com>
2024-04-18 18:08:53 +0200

    setting up sequelize

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const { sequelize, User } = require('./models')
+
+const app = express()
+app.use(express.json())
+
+
+
+app.post('/users', async (req, res) => {
+    const {name, email, role} = req.body;
+
+    try {
+        const user = await User.create({name, email, role})
+
+        return res.json(user)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({error})
+    }
+})
+
+app.listen(8080, async ()=>{
+    console.log('Server is running on: http://localhost:8080')
+    await sequelize.sync({force: true})
+    console.log("Database connected")
+})
+
